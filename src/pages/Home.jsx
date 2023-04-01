710a55355c69effb92cc2abd1f5fea25e76f7ba1
amankashyap004 <amankashyap004@gmail.com>
2023-04-01 18:42:22 +0530

    add store file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
+import state from "../store";
 import {
    headContainerAnimation,
    headContentAnimation,
@@ -9,6 +10,7 @@ import {
 } from "../config/motion";
 
 const Home = () => {
+   const snap = useSnapshot(state);
    return <div>Home</div>;
 };
 
