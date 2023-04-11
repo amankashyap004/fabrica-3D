3f200e1ed78514aeb4f8902d08f1546f9daa78b7
amankashyap004 <amankashyap004@gmail.com>
2023-04-11 20:25:18 +0530

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,30 @@ import {
 
 const Home = () => {
    const snap = useSnapshot(state);
-   return <div>Home</div>;
+   return (
+      <AnimatePresence>
+         {snap.intro && (
+            <motion.section className="home" {...slideAnimation("left")}>
+               <motion.header {...slideAnimation("down")}>
+                  <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain" />
+               </motion.header>
+               <motion.div className="home-content" {...headContainerAnimation}>
+                  <motion.div {...headTextAnimation}>
+                     <h1 className="head-text">
+                        LET'S <br className="xl:block hidden" /> DO IT.
+                     </h1>
+                  </motion.div>
+                  <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+                     <p className="max-w-md font-normal text-gray-600 text-base">
+                        Create your unique and exclusive shirt with our brand-new 3D customization
+                        tool. <strong>Unleash your imagination</strong> and define your own style.
+                     </p>
+                  </motion.div>
+               </motion.div>
+            </motion.section>
+         )}
+      </AnimatePresence>
+   );
 };
 
 export default Home;
