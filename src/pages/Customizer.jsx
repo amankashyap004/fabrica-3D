e2882c17a091288b7751090734bf913685266fc9
amankashyap004 <amankashyap004@gmail.com>
2023-04-12 20:32:08 +0530

    side tab in Customizer

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -11,7 +11,32 @@ import { fadeAnimation, slideAnimation } from "../config/motion";
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from "../components";
 
 const Customizer = () => {
-   return <div>customizer</div>;
+   const snap = useSnapshot(state);
+   return (
+      <AnimatePresence>
+         {!snap.intro && (
+            <>
+               <motion.div
+                  key="custom"
+                  className="absolute top-0 left-0 z-10"
+                  {...slideAnimation("left")}
+               >
+                  <div className="flex items-center min-h-screen">
+                     <div className="editortabs-container tabs">
+                        {EditorTabs.map((tab) => (
+                           <Tab
+                              key={tab.name}
+                              tab={tab}
+                              handleClick={() => setActiveEditorTab(tab.name)}
+                           />
+                        ))}
+                     </div>
+                  </div>
+               </motion.div>
+            </>
+         )}
+      </AnimatePresence>
+   );
 };
 
 export default Customizer;
