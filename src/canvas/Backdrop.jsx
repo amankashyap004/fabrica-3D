efafee0919dd6ccd4d57b2767ba32dd2fe458437
amankashyap004 <amankashyap004@gmail.com>
2023-04-15 19:07:35 +0530

    canvas Backdrop

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
-   return <div>Backdrop</div>;
+   const shadows = useRef();
+
+   return (
+      <AccumulativeShadows position={[0, 0, -0.14]}>
+         <RandomizedLight amount={4} />
+      </AccumulativeShadows>
+   );
 };
 
 export default Backdrop;
